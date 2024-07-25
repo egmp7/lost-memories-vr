61f111267c01965a15cf1fec6ab66b9dd2c6bd95
egmp7 <deweyerick@gmail.com>
2024-07-24 20:11:42 -0500

    footsteps and door sounds

---
+++ Assets/Scripts/Footsteps.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    [SerializeField] Transform Player ;
+    [SerializeField] float frequency = 1.0f;
+
+    private Vector3 _tempPosition;
+    private AudioSource _audioSource;
+    
+    void Start()
+    {
+        _tempPosition = Player.position;
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        Vector2 tempPos = new Vector2(_tempPosition.x, _tempPosition.z);
+        Vector2 playerPos = new Vector2(Player.position.x, Player.position.z); 
+
+        float distance = Vector2.Distance(tempPos, playerPos); 
+
+        if (distance > frequency)
+        {
+            _audioSource.Play();
+            _tempPosition = Player.position;
+        }
+    }
+}
+
--- Assets/Scripts/OpenAndCloseDoor.cs
+++ Assets/Scripts/OpenAndCloseDoor.cs
@@ -3,12 +3,20 @@ using UnityEngine;
 
 public class OpenAndCloseDoor : MonoBehaviour
 {
+    [Header("Door")]
     [SerializeField] bool isDoorOpen;
+
+    [Header("Audio")]
+    [SerializeField] AudioClip openDoorSound;
+    [SerializeField] AudioClip closeDoorSound;
+
     private Animator animator;
+    private AudioSource audioSource;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void TriggerDoor()
@@ -26,7 +34,16 @@ public class OpenAndCloseDoor : MonoBehaviour
     IEnumerator opening()
     {
         print("you are opening the door");
+        // Play opening animation
         animator.Play("Opening");
+
+        // Play open door sound
+        if (audioSource != null && openDoorSound != null)
+        {
+            audioSource.clip = openDoorSound;
+            audioSource.Play();
+        }
+
         isDoorOpen = true;
         yield return new WaitForSeconds(.5f);
     }
@@ -34,7 +51,14 @@ public class OpenAndCloseDoor : MonoBehaviour
     IEnumerator closing()
     {
         print("you are closing the door");
+        // Play closing animation
         animator.Play("Closing");
+        // Play close door sound
+        if (audioSource != null && closeDoorSound != null)
+        {
+            audioSource.clip = closeDoorSound;
+            audioSource.Play();
+        }
         isDoorOpen = false;
         yield return new WaitForSeconds(.5f);
     }
